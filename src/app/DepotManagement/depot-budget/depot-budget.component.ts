fe1d68b5e63fcec1909b0f7d3b6c1e4bb2fe0930
Md.Jahirul Islam Shozib <mshozib000@gmail.com>
2023-01-01 11:55:40 +0600

    depot-management added

--- src/app/DepotManagement/depot-budget/depot-budget.component.ts
+++ src/app/DepotManagement/depot-budget/depot-budget.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { DataStorageService } from 'src/app/Service/Data Fetch & Store/data-storage.service';
+import { DataModel } from 'src/app/shared/data.model';
 
 @Component({
   selector: 'app-depot-budget',
   templateUrl: './depot-budget.component.html',
-  styleUrls: ['./depot-budget.component.scss']
+  styleUrls: ['./depot-budget.component.scss'],
 })
 export class DepotBudgetComponent implements OnInit {
+  depotId!: string;
+  dataItems: DataModel[] = [];
+  constructor(private dataStorageService: DataStorageService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  sendDepotId() {
+    console.log(this.depotId);
+    this.fetchDepotId(this.depotId);
   }
 
+  fetchDepotId(code: string) {
+    this.dataStorageService.fetchDepot(code).subscribe({
+      next: (response: any) => {
+        console.log('response:: ', response);
+        this.dataItems = response;
+      },
+      error: (err: any) => {
+        console.log(err);
+        this.dataItems = [];
+      },
+    });
+  }
 }
